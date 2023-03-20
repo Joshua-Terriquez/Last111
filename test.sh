18ed72dd6dc26167f3a640a7284b54060f549bde
Joshua Terriquez <74273335+Joshua-Terriquez@users.noreply.github.com>
2023-03-19 17:32:56 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #java
-echo "JAVA"
-javac Pfile.java
-java -classpath ".:sqlite-jdbc-3.32.3.2.jar" Pfile
+#echo "JAVA"
+#javac Pfile.java
+#java -classpath ".:sqlite-jdbc-3.32.3.2.jar" Pfile
 
 #python
 echo ""
